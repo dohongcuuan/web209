13ef925123a33d0c4c8e652f37c8dea04c5d82be
hongquando4623 <dohongquan459@gmail.com>
2023-06-29 23:42:07 +0700

    Buổi 2: remove

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,15 +1,20 @@
 import { ICar } from "@/interfaces/car";
+import { GoTrash } from "react-icons/go";
 import { Button } from "..";
 import styles from "./Item.module.css";
 
 type ItemProps = {
     car: ICar;
+    onRemove: (id: number) => void;
 };
 
-const Item = ({ car }: ItemProps) => {
+const Item = ({ car, onRemove }: ItemProps) => {
     return (
         <li className={styles.item}>
-            {car.name} <Button />
+            {car.name}
+            <Button danger onClick={() => onRemove(car.id!)}>
+                <GoTrash />
+            </Button>
         </li>
     );
 };
